dbf557de7aacc40f22be0bc0088848e578e95f9f
prathamesh201999 <63088560+prathamesh201999@users.noreply.github.com>
2020-04-06 10:21:57 +0530

    Binary search

---
+++ Binary_search.py
@@ -0,0 +1,23 @@
+pos = -1
+
+def search(list,n):
+    l = 0
+    u = len(list)
+    for i in list:
+        mid = (l+u) // 2
+        if list[mid] == n:
+            globals()['pos'] = mid
+            return True
+        else:
+            if list[mid] < n:
+                l = mid + 1
+            else:
+                u = mid - 1
+    return False
+
+list = [1,2,3,4,6,7,8,9]
+n = 5
+if search(list, n):
+    print("Found at :",pos+1)
+else:
+    print("Not Found")
